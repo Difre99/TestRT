7b34e175fab3751202f500fbf9f539b71fb2f39f
Marco di Fresco <marco.difresco@metide.com>
2022-04-14 17:47:11 +0200

    add test for understanding dispose

--- TestRXSwift/TestRXSwift/ViewController.swift
+++ TestRXSwift/TestRXSwift/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        testDispose()
+        
     }
 
+    func testDispose(){
+        let scheduler = SerialDispatchQueueScheduler(qos: .default)
+        let subscription = Observable<Int>.interval(.milliseconds(500), scheduler: scheduler)
+            .subscribe { event in
+                print(event)
+            }
+        print("CIAO1")
+        Thread.sleep(forTimeInterval: 2.0)
+        print("CIAO2")
+        subscription.dispose()
+        print("CIAO3")
+        
+        /*
+         out:
+         CIAO1
+         next(0)
+         next(1)
+         next(2)
+         next(3)
+         CIAO2
+         CIAO3
+         */
+    }
 
 }
 
