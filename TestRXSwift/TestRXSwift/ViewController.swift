75cd862f3d92b478f28a56cd689d3b550a94233e
Marco di Fresco <marco.difresco@metide.com>
2022-04-15 13:36:05 +0200

    dispose with button

--- TestRXSwift/TestRXSwift/ViewController.swift
+++ TestRXSwift/TestRXSwift/ViewController.swift
@@ -9,25 +9,34 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
+  
+    @IBOutlet weak var disposeButton: UIButton!
+    
+    var subscription: Disposable?
+    var a: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpButton()
         testDispose()
         
     }
-
+    
+    func setUpButton(){
+        self.disposeButton.titleLabel?.text = "Dispose"
+    }
+    
     func testDispose(){
         let scheduler = SerialDispatchQueueScheduler(qos: .default)
-        let subscription = Observable<Int>.interval(.milliseconds(500), scheduler: scheduler)
+        self.subscription = Observable<Int>.interval(.milliseconds(500), scheduler: scheduler)
             .subscribe { event in
+                self.a += 1
                 print(event)
             }
         print("CIAO1")
         Thread.sleep(forTimeInterval: 2.0)
         print("CIAO2")
-        subscription.dispose()
         print("CIAO3")
-        
         /*
          out:
          CIAO1
@@ -39,6 +48,17 @@ class ViewController: UIViewController {
          CIAO3
          */
     }
-
+    
+    @IBAction func disposeButtonTouchUpInside(_ sender: Any) {
+        self.subscription?.dispose()
+        let alert = UIAlertController(title: "Stop with dispose", message: "Not the best way to use observables.\n Number of events = \(self.a)", preferredStyle: UIAlertController.Style.alert)
+        let dismissAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+            UIAlertAction in
+            print("Yes Pressed")
+        }
+        alert.addAction(dismissAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
